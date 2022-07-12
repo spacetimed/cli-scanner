818c44e426c4f29b39144c0043d59949a40fc793
FFFFFF-base16 <s127813127@gmail.com>
2022-07-12 16:27:17 -0700

    added -H --hash flag support

--- scanner/__main__.py
+++ scanner/__main__.py
@@ -1,9 +1,19 @@
 import argparse
 import os
+from sys import exit
 from scanlib.main import Scanner
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--file', type=os.path.abspath, help='relative file name', required=True)
+group = parser.add_mutually_exclusive_group(required=True)
+
+group.add_argument('-f', '--file', type=os.path.abspath, help='relative file name')
+group.add_argument('-H', '--hash', type=str, help='hash')
 args = parser.parse_args()
 
-Scanner(args.file)
+if args.file is not None:
+    Scanner(args.file, which='file')
+
+elif args.hash is not None:
+    Scanner(args.hash, which='hash')
+
+exit()
--- scanner/scanlib/main.py
+++ scanner/scanlib/main.py
@@ -7,13 +7,19 @@ import hashlib
 from colorama import Fore, Back, Style
 from datetime import datetime
 
-from typing import Optional, Union
+from typing import Optional
+from typing import Union
+from typing import Any 
 
 class Logger:
     def __init__(self) -> None:
         return None
     
-    def __call__(self, message: str) -> bool:
+    def __call__(self, msg: str, tag: str = None) -> bool:
+        print('  ', end='')
+        if(tag == 'error'):
+            print(Back.RED, Fore.WHITE, 'error  ', end=Style.RESET_ALL)
+        print(' ', msg)
         return
         
 class APIRequest:
@@ -44,25 +50,39 @@ class APIRequest:
         if not response.ok:
             print('error1', response.text)
             return None 
+
+        # with open('./tmp/output', 'w') as f:
+        #    f.write(response.text)
         
         return json.loads(response.text)
 
 
 class Scanner:
-    def __init__(self, path: str) -> None:
-        self.master_dir = os.path.dirname(os.path.realpath(__file__))
-        self.APIRequest = APIRequest(config.Config(self.master_dir))
-        self.Log = Logger()
-        if os.path.exists(path):
+    def __init__(self, __target: str, which: str) -> None:
+
+        self.master_dir: str = os.path.dirname(os.path.realpath(__file__))
+        self.config: Any = config.Config(self.master_dir)
+        self.APIRequest: Any = APIRequest(self.config)
+        self.Log: Any = Logger()
+        path: Optional[str] = None
+        hash: Optional[str] = None
+        
+        if(which == 'file'):
+            path = __target
+            if not os.path.exists(path):
+                return self.Log(f'File does not exist ({path})', tag='error')
+            if os.path.getsize(path) > self.config.size_limit:
+                return self.Log(f'File size is too large', tag='error')
             hash = self.get_file_hash(path)
-            self.scan_file_by_hash(hash)
-        else:
-            print('doesnt exist')
-        return
+
+        elif(which == 'hash'):
+            hash = __target
+
+        self.scan_file_by_hash(hash)
 
     def get_file_hash(self, path: str):
         with open(path, "rb") as f:
-            f_byte= f.read()
+            f_byte = f.read()
             result = hashlib.sha256(f_byte)
             return result.hexdigest()
 
@@ -88,6 +108,9 @@ class Scanner:
         last_submission_date = attr['last_submission_date']
         first_submission_date = attr['first_submission_date']
         last_analysis_stats = attr['last_analysis_stats']
+        names = attr['names'][0:6]
+        format_names = ', '.join(names)
+        type = attr['type_description']
         
         print()
         print()
@@ -95,9 +118,11 @@ class Scanner:
         print()
         print(Fore.BLUE, f'  https://www.virustotal.com/gui/file/{scan_id}', Style.RESET_ALL)
         print()
+        self.row('type', type)
+        self.row('names', f'{format_names}, ...')
         self.row('submissions', times_submitted)
         self.row('votes', f"{total_votes['harmless']} 👍  / {total_votes['malicious']} 👎")
-        self.row('analysis', f"{last_analysis_stats['malicious']} / {last_analysis_stats['undetected'] + last_analysis_stats['malicious']}", flag=(last_analysis_stats['malicious'] > 0))
+        self.row('detections', f"{last_analysis_stats['malicious']} / {last_analysis_stats['undetected'] + last_analysis_stats['malicious']} {'❌' if (last_analysis_stats['malicious'] > 0) else '✅'}", flag=(last_analysis_stats['malicious'] > 0))
         self.row('first seen', self.format_timestamp(first_submission_date))
         self.row('last seen', self.format_timestamp(last_submission_date))
         print()
