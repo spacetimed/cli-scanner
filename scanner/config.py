9f104046ec6f25e54a28b0cd41b2aed722d33672
FFFFFF-base16 <s127813127@gmail.com>
2022-07-12 20:01:38 -0700

    improved typehints

--- scanner/config.py
+++ scanner/config.py
@@ -1,12 +1,7 @@
-class Config:
-    def __init__(self, master_dir: str) -> None:
+from typing import Dict
 
-        with open(f'{master_dir}/API_KEY', 'r') as f:
-            self.api_key = f.readline()
-        
-        self.master_dir = master_dir
-        self.endpoint = 'https://www.virustotal.com/api/v3'
-        self.device_name = 'test'
-        self.size_limit = 12000
-
-        return
+options: Dict[str, str] = {
+    'endpoint' : 'https://www.virustotal.com/api/v3',
+    'device_name' : 'test',
+    'size_limit' : 12000
+}
--- scanner/scanlib/main.py
+++ scanner/scanlib/main.py
@@ -1,21 +1,24 @@
-import config
 import requests
 import json
 import os
 import hashlib
 
+from config import options as __options__
 from colorama import Fore, Back, Style
 from datetime import datetime
 
+from typing import List
 from typing import Optional
 from typing import Union
 from typing import Any 
+from typing import Type
+from typing import Dict
 
 class Logger:
     def __init__(self) -> None:
         return None
     
-    def __call__(self, msg: str, tag: str = None) -> bool:
+    def __call__(self, msg: str, tag: Optional[str] = None) -> None:
         print('  ', end='')
         if(tag == 'error'):
             print(Back.RED, Fore.WHITE, 'error  ', end=Style.RESET_ALL)
@@ -23,36 +26,39 @@ class Logger:
         return
         
 class APIRequest:
-    def __init__(self, config: type) -> None:
-        self.config = config
-        self.master_dir = config.master_dir
-        self.headers = {
+    def __init__(self, config: Dict[str, str]) -> None:
+        self.Log: Type[Logger] = Logger()
+        self.config: Dict[str, str] = config 
+        self.headers: Dict[str, str] = {
             "Accept": "application/json",
-            "x-apikey": self.config.api_key
+            "x-apikey": self.config['api_key'] 
         }
         return
 
-    def __call__(self, route: str, method: str, add_headers: dict = None) -> Optional[requests.models.Response]:
-        url = f'{self.config.endpoint}/{route}'
-        headers = self.headers
+    def __call__(
+        self, 
+        route: str, 
+        method: str, 
+        add_headers: Dict[str, str] = None
+    ) -> Optional[requests.models.Response]:
+
+        url: str = f"{self.config['endpoint']}/{route}"
+        headers: Dict[str, str] = self.headers
         
         if add_headers is not None:
             headers.update(add_headers)
 
         if(method == 'post'):
-            print('error1')
+            self.Log('Invalid request', tag='error')
             return None
 
         elif(method == 'get'):
             response = requests.get(url, headers=headers)
 
-
         if not response.ok:
+            self.Log('Invalid API response', tag='error')
             print('error1', response.text)
             return None 
-
-        # with open('./tmp/output', 'w') as f:
-        #    f.write(response.text)
         
         return json.loads(response.text)
 
@@ -61,9 +67,16 @@ class Scanner:
     def __init__(self, __target: str, which: str) -> None:
 
         self.master_dir: str = os.path.dirname(os.path.realpath(__file__))
-        self.config: Any = config.Config(self.master_dir)
-        self.APIRequest: Any = APIRequest(self.config)
-        self.Log: Any = Logger()
+
+        with open(f'{self.master_dir}/API_KEY', 'r') as f:
+            self.api_key = f.readline() 
+
+        self.config: Dict[str, str] = __options__ 
+        self.config['api_key'] = self.api_key
+
+        self.APIRequest: Type[APIRequest] = APIRequest(self.config)
+        self.Log: Type[Logger] = Logger()
+
         path: Optional[str] = None
         hash: Optional[str] = None
         
@@ -71,7 +84,7 @@ class Scanner:
             path = __target
             if not os.path.exists(path):
                 return self.Log(f'File does not exist ({path})', tag='error')
-            if os.path.getsize(path) > self.config.size_limit:
+            if os.path.getsize(path) > self.config['size_limit']:
                 return self.Log(f'File size is too large', tag='error')
             hash = self.get_file_hash(path)
 
@@ -82,12 +95,12 @@ class Scanner:
 
     def get_file_hash(self, path: str):
         with open(path, "rb") as f:
-            f_byte = f.read()
-            result = hashlib.sha256(f_byte)
+            f_byte: bytes = f.read()
+            result: hashlib._Hash = hashlib.sha256(f_byte)
             return result.hexdigest()
 
     def row(self, title: str, data: Union[str, int], flag: bool = False) -> None:
-        padding = 18
+        padding: int = 18
         title += ':'
         if flag:
             data = Fore.RED + data
@@ -95,25 +108,29 @@ class Scanner:
 
 
     def format_timestamp(self, timestamp: int) -> str:
-        format = datetime.fromtimestamp(timestamp)
+        format: datetime = datetime.fromtimestamp(timestamp)
         return(f"{format.strftime('%c')}")
 
 
-    def display_result(self, response: dict) -> None:
+    def display_result(self, response: Dict[str, str]) -> None:
 
-        scan_id = response['data']['id']
-        attr = response['data']['attributes']
-        times_submitted = attr['times_submitted']
-        total_votes = attr['total_votes']
-        last_submission_date = attr['last_submission_date']
-        first_submission_date = attr['first_submission_date']
-        last_analysis_stats = attr['last_analysis_stats']
-        names = attr['names'][0:6]
-        format_names = ', '.join(names)
-        type = attr['type_description']
+        scan_id: str = response['data']['id']
+        attr: str = response['data']['attributes']
+
+        names: List[str] = attr['names'][0:6]
+        type: str = attr['type_description']
+
+        times_submitted: int = attr['times_submitted']
+        total_votes: Dict[str, int] = attr['total_votes']
+
+        last_submission_date: int = attr['last_submission_date']
+        first_submission_date: int = attr['first_submission_date']
+
+        last_analysis_stats: Dict[str, int] = attr['last_analysis_stats']
+
+        format_names: str = ', '.join(names)
         
-        print()
-        print()
+        print('\n')
         print(Back.WHITE, Fore.BLACK, 'SCAN COMPLETE', Style.RESET_ALL)
         print()
         print(Fore.BLUE, f'  https://www.virustotal.com/gui/file/{scan_id}', Style.RESET_ALL)
@@ -125,35 +142,19 @@ class Scanner:
         self.row('detections', f"{last_analysis_stats['malicious']} / {last_analysis_stats['undetected'] + last_analysis_stats['malicious']} {'❌' if (last_analysis_stats['malicious'] > 0) else '✅'}", flag=(last_analysis_stats['malicious'] > 0))
         self.row('first seen', self.format_timestamp(first_submission_date))
         self.row('last seen', self.format_timestamp(last_submission_date))
-        print()
-        print()
+        print('\n')
 
         return
 
 
     def scan_file_by_hash(self, hash: str) -> None:
-        response = self.APIRequest(f'files/{hash}', 'get') # SHA-256, SHA-1 or MD5
+        
+        # supported hashes: SHA-256, SHA-1, MD5
+        response: Optional[requests.models.Response] = self.APIRequest(f'files/{hash}', 'get') 
 
         if not response:
-            self.Log('An error occurred.')
+            self.Log('An error occurred [2]', tag='error')
             return
 
         self.display_result(response)
-
-        """
-            times_submitted
-            total_votes
-            last_submission_date
-            first_submission_date
-            "last_analysis_stats": {
-                "harmless": 0,
-                "type-unsupported": 3,
-                "suspicious": 0,
-                "confirmed-timeout": 0,
-                "timeout": 0,
-                "failure": 0,
-                "malicious": 55,
-                "undetected": 14
-            },
-            "id"
-        """
+        return
